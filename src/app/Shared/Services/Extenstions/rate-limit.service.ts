f3d9add3eab8bcfcc4e02cf5b164082c0db2312c
Grace <Yvesb@live.be>
2021-03-11 17:48:49 +0100

    Extracted RateLimit to its own Service

---
+++ src/app/Shared/Services/Extenstions/rate-limit.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
+import { timer } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RateLimitService {
+  constructor(private _http: HttpClient) {}
+
+  async fetchData<T>(url: string, retries = 3, timeout = 0): Promise<T> {
+    if (retries > 0) {
+      if (timeout != 0) {
+        console.log('Rate-limited: ', timeout / 1000 + 's');
+        await timer(timeout).pipe(take(1)).toPromise();
+      }
+      return await this._http
+        .get<T>(url, { observe: 'response' })
+        .toPromise()
+        .then((res: HttpResponse<T>) => {
+          return res.body;
+        })
+        .catch((e: HttpErrorResponse) => {
+          if (e.status == 429) {
+            let time = Math.ceil(
+              Number(e.headers.get('x-ratelimit-reset')) - Date.now() / 1000
+            );
+            return this.fetchData(url, retries - 1, time * 1000);
+          }
+          return this.fetchData(url, retries - 1, 1000);
+        });
+    } else {
+      throw new Error('Something went wrong with getting data');
+    }
+  }
+}
